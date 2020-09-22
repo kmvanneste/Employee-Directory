d06081b72cd6dede45b0d73d09f5f6db342d17f8
kmvanneste <kmvanneste17@gmail.com>
2020-09-22 00:40:02 -0400

    cleared errors in console for SearchResults

--- react-employee-directory/src/components/SearchResults/index.js
+++ react-employee-directory/src/components/SearchResults/index.js
@@ -4,25 +4,29 @@ import "./style.css";
 export default function SearchResults( {employees} ) {
   return (
     <table>
-    <tr>
-        <th>Photo</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>Date of Birth</th>
-    </tr>
+      <thead>
+          <tr>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Date of Birth</th>
+          </tr>
+    </thead>
       {employees[0]!== undefined && employees[0].name !==undefined ? (
       employees.map(({login, picture, name, email, phone, dob}) => {
           return (
-        <td className="list-group-item" key={login.uuid}>
-            <td>
-                <img alt={`employee`} src={picture.thumbnail}></img>
-            </td>
-            <td>{name.title} {name.first} {name.last}</td>
-            <td><a href={"mailto:" + email}>{email}</a></td>
-            <td>{phone}</td>
-            <td>{dob.date.substring(0, 10)}</td>
-        </td> 
+       <tbody key={login.uuid}>     
+          <tr className="list-group-item" >
+              <td>
+                  <img alt={`employee`} src={picture.thumbnail}></img>
+              </td>
+              <td>{name.title} {name.first} {name.last}</td>
+              <td><a href={"mailto:" + email}>{email}</a></td>
+              <td>{phone}</td>
+              <td>{dob.date.substring(0, 10)}</td>
+          </tr> 
+        </tbody>
           )})): 
           (
             <></>
