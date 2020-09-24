b9607865259cea92fdc9c15eb1a031447fe431c6
kmvanneste <kmvanneste17@gmail.com>
2020-09-23 20:17:01 -0400

    Updates

---
+++ precache-manifest.3f7ee8cb77bb7b08caaccd98ccb14f7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f57a1f36ae2edd80492791ab50076d",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "49fb3566ef69ddfbabdc",
+    "url": "/Employee-Directory/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "53ad1b41c9e39d96cdc5",
+    "url": "/Employee-Directory/static/css/main.96c3ba7d.chunk.css"
+  },
+  {
+    "revision": "49fb3566ef69ddfbabdc",
+    "url": "/Employee-Directory/static/js/2.14be0d78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.14be0d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ad1b41c9e39d96cdc5",
+    "url": "/Employee-Directory/static/js/main.1cedc77e.chunk.js"
+  },
+  {
+    "revision": "80e2965fc89510d843ac",
+    "url": "/Employee-Directory/static/js/runtime-main.1567a3b2.js"
+  }
+]);
